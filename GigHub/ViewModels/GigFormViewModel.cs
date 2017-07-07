30113038078f457ba6610e6d01a2c570f2b555f8
Hien Le <lhien11@gmail.com>
2017-07-07 16:18:40 -0500

    Implement saving a gig.

--- GigHub/ViewModels/GigFormViewModel.cs
+++ GigHub/ViewModels/GigFormViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using GigHub.Models;
+using System;
 
 namespace GigHub.ViewModels
 {
@@ -8,7 +9,12 @@ namespace GigHub.ViewModels
         public string Venue { get; set; }
         public string Date { get; set; }
         public string Time { get; set; }
-        public int Genre { get; set; }
+        public byte Genre { get; set; }
         public IEnumerable<Genre> Genres { get; set; }
+
+        public DateTime DateTime
+        {
+            get { return DateTime.Parse(string.Format("{0} {1}", Date, Time)); }
+        }
     }
 }
